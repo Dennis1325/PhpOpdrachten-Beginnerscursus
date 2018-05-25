b5b9b5f000cec28d9ef8ac7bdb402cbf7a67aea5
Dennis <dennis1325@live.nl>
2018-05-25 10:24:11 +0200

    Php Opdrachten t/m reisbusje

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+// VARIABELEN WEERGEVEN
+//$aantal = 14;
+//$deze_jongen = "Deze jongen is ";
+//
+//echo $deze_jongen.$aantal." jaar oud!";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Php Opdrachten</title>
+</head>
+<style>
+    body{
+        text-align: center;
+    }
+    .rood{
+        border: red solid 5px;
+    }
+    .groen{
+        border: green solid 5px;
+    }
+
+
+</style>
+<body>
+<?php
+
+// AAP FOR LOOP
+//for ($i = 1; $i < 6; $i++){
+//    echo "<img".$class." src='Img/apen/aap".$i.".jpg'>";
+//}
+
+// CONDITIES
+//for ($i = 1; $i < 9; $i++){
+//    if ($i % 2 == 0){
+//        $class = "class='rood'";
+//    }
+//    else{
+//        $class = "class='groen'";
+//    }
+//    echo "<img ".$class."src='Img/apen/aap".$i.".jpg'>";
+//}
+
+// FOREACH BOMEN
+//$bomen = array("img_0050.jpg","lillypilly1.jpg","Maranchery_Biyyam_Kayal_kandal.jpg");
+//
+//foreach($bomen as $boom){
+//    echo "<img src='Img/bomen/".$boom."'>";
+//}
+
+// KERSTBOOM LOOP
+//for($i = 0; $i <= 50; $i++){
+//    for ($j = 0; $j < $i; $j++){
+//        echo "*";
+//    }
+//    echo "*<br>";
+//}
+
+
+// BUSREIS OPDRACHT
+//$leeftijd = 50;
+//$bedrag = 10;
+//if ($leeftijd > 65 || $leeftijd <= 12){
+//    $bedrag = $bedrag * 0.5;
+//}
+//if ($leeftijd < 3){
+//    $bedrag = 0;
+//}
+//echo $bedrag;
+
+
+?>
+
+</body>
+</html>
